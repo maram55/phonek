7f4be3c8cb8bed9b8c6af46223977d7d5cfac56e
maram55 <36778696+maram55@users.noreply.github.com>
2020-04-09 22:44:53 +0200

    intial commit

---
+++ emarket/Models/Extended/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace emarket.Models
+{
+    [MetadataType(typeof(CategoryMetadata))]
+    public partial class Category
+    {
+    }
+
+    public class CategoryMetadata
+    {
+         [Display(Name = "id")]
+         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Id")]
+        public int Id { get; set; }
+
+
+        [Display(Name = "Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+
+
+
+        [Display(Name = "Number of product")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Number of prod")]
+        public int No_Of_Product { get; set; }
+
+
+    }
+}
---
+++ emarket/Models/Extended/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace emarket.Models
+{
+       [MetadataType(typeof(ProductMetadata))]
+    public partial class Product
+    {
+
+    }
+
+    public class ProductMetadata
+    {
+
+        // [Display(Name = "ID")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Please enter ID")]
+        //public int Id { get; set; }
+
+
+        [Display(Name = "Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Price")]
+        public double Price { get; set; }
+
+
+        [Display(Name = "Image")]
+        public byte[] Image { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Descripyion")]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Category_Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter Category_Id")]
+        public int Category_Id { get; set; }
+
+
+    }
+
+}
+}
